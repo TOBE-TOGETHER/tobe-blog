c5fed216eb0179b7466aae706086fe4079e230b6
Lucien Chen <474504303@qq.com>
2024-07-19 23:46:50 +0800

    update loading background color

--- tobe_blog_web/src/components/loading/Loading.tsx
+++ tobe_blog_web/src/components/loading/Loading.tsx
@@ -10,6 +10,7 @@ export default function Loading(props: Readonly<Props>) {
       sx={{
         color: theme => theme.palette.primary.light,
         zIndex: theme => theme.zIndex.drawer + 1,
+        backgroundColor: theme => theme.palette.common.white,
       }}
       open={props.open || false}
     >
