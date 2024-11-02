a418c771e1771fcceb930e0cd824fa6ee5b2ddc8
Lucien Chen <474504303@qq.com>
2024-11-02 23:50:54 +0800

    Fixed compile issue

--- tobe_blog_web/src/portal/components/RelevantContentsPanel.tsx
+++ tobe_blog_web/src/portal/components/RelevantContentsPanel.tsx
@@ -1,9 +1,9 @@
 import { Divider, Grid, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { IBaseUserContentDTO } from '../../global/types';
 import * as PublicDataService from '../../services/PublicDataService';
 import RelevantContentItem from './RelvantContentIteam';
-import { useTranslation } from 'react-i18next';
 
 export default function RelevantContentsPanel(props: Readonly<{ content: IBaseUserContentDTO }>) {
   const { t } = useTranslation();
@@ -14,7 +14,7 @@ export default function RelevantContentsPanel(props: Readonly<{ content: IBaseUs
       props.content.contentType,
       100,
       1,
-      props.content.tags.map(t => t.value),
+      props.content.tags.map(t => Number.parseInt(t.value)),
       ''
     ).then(response => {
       let records = response.data.records.filter((r: IBaseUserContentDTO) => r.id !== props.content.id);
@@ -23,12 +23,24 @@ export default function RelevantContentsPanel(props: Readonly<{ content: IBaseUs
   }, [props.content]);
 
   return relevantContents.length > 0 ? (
-    <Grid container sx={{ mt: 10 }} justifyContent='center'>
+    <Grid
+      container
+      sx={{ mt: 10 }}
+      justifyContent="center"
+    >
       <Divider>
-        <Typography variant='h5' color='textSecondary'>
-        {t('components.relevant-contents')}</Typography>
+        <Typography
+          variant="h5"
+          color="textSecondary"
+        >
+          {t('components.relevant-contents')}
+        </Typography>
       </Divider>
-      <Grid container spacing={2} sx={{ mt: 5 }}>
+      <Grid
+        container
+        spacing={2}
+        sx={{ mt: 5 }}
+      >
         {relevantContents.map((c: IBaseUserContentDTO) => (
           <RelevantContentItem
             content={c}
