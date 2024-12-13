e31bd799db2c093d578d299bd3b3f20a3b2c7a3b
Lucien Chen <474504303@qq.com>
2024-12-13 09:36:18 +0800

    Optimize infinite loading performance

--- tobe_blog_web/src/portal/components/RelevantContentsPanel.tsx
+++ tobe_blog_web/src/portal/components/RelevantContentsPanel.tsx
@@ -1,26 +1,36 @@
 import { Divider, Grid, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { InfiniteScrollList } from '../../components';
 import { IBaseUserContentDTO } from '../../global/types';
 import * as PublicDataService from '../../services/PublicDataService';
 import RelevantContentItem from './RelvantContentIteam';
 
 export default function RelevantContentsPanel(props: Readonly<{ content: IBaseUserContentDTO }>) {
   const { t } = useTranslation();
+  const DEFAULT_PAGE_SIZE: number = 12;
   const [relevantContents, setRelevantContents] = useState<IBaseUserContentDTO[]>([]);
+  const [current, setCurrent] = useState<number>(0);
+  const [totalPage, setTotalPage] = useState<number>(1);
 
   useEffect(() => {
+    loadData({ reset: true });
+  }, [props.content]);
+
+  function loadData(option: { reset: boolean }): void {
     PublicDataService.getNewsByTags(
       props.content.contentType,
-      100,
-      1,
+      DEFAULT_PAGE_SIZE,
+      option.reset ? 1 : current + 1,
       props.content.tags.map(t => Number.parseInt(t.value)),
       ''
     ).then(response => {
       let records = response.data.records.filter((r: IBaseUserContentDTO) => r.id !== props.content.id);
-      setRelevantContents(records);
+      setRelevantContents(relevantContents.concat(records));
+      setCurrent(response.data.current);
+      setTotalPage(response.data.pages);
     });
-  }, [props.content]);
+  }
 
   return relevantContents.length > 0 ? (
     <Grid
@@ -41,12 +51,19 @@ export default function RelevantContentsPanel(props: Readonly<{ content: IBaseUs
         spacing={2}
         sx={{ mt: 5 }}
       >
-        {relevantContents.map((c: IBaseUserContentDTO) => (
-          <RelevantContentItem
-            content={c}
-            key={`relevant-item-${c.id}`}
-          />
-        ))}
+        <InfiniteScrollList
+          loading={false}
+          option={{ reset: false }}
+          dataSource={relevantContents}
+          hasMore={current < totalPage}
+          loadMore={loadData}
+          renderItem={(c: IBaseUserContentDTO) => (
+            <RelevantContentItem
+              content={c}
+              key={`relevant-item-${c.id}`}
+            />
+          )}
+        />
       </Grid>
     </Grid>
   ) : (
