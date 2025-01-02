4f0c2561b97524de963106537d6463bc4118eae7
Lucien Chen <474504303@qq.com>
2025-01-03 00:50:48 +0800

    fix sonar issue

--- tobe_blog_web/src/admin/containers/content/components/ContentPage.tsx
+++ tobe_blog_web/src/admin/containers/content/components/ContentPage.tsx
@@ -2,11 +2,13 @@ import { ReactNode } from 'react';
 import { Page } from '../../../components';
 import ContentEditBar from './ContentEditBar';
 
-export default function BaseContentPage(props: { children?: ReactNode[] | ReactNode; title: string; loading: boolean; editable: boolean; handleEditableChange: () => void }) {
+export default function BaseContentPage(
+  props: Readonly<{ children?: ReactNode[] | ReactNode; title: string; loading: boolean; editable: boolean; handleEditableChange: () => void }>
+) {
   return (
     <Page
       openLoading={props.loading}
-      pageTitle={props.title || ''}
+      pageTitle={props.title}
     >
       <ContentEditBar
         editable={props.editable}
