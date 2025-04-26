1cf5bd293beedd59f7444b33ba34cdd91197abc1
Lucien Chen <474504303@qq.com>
2025-04-26 14:55:14 +0800

    Implement mail sending

--- tobe_blog_service/src/test/java/com/tobe/blog/ApplicationTests.java
+++ tobe_blog_service/src/test/java/com/tobe/blog/ApplicationTests.java
@@ -1,7 +1,11 @@
 package com.tobe.blog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
+@ActiveProfiles("test")
 class ApplicationTests {
 
     @Test
