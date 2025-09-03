1d39daf80037beb404afd2932f8f44ae2e33030d
Lucien Chen <474504303@qq.com>
2025-09-03 22:35:41 +0800

    adjust UI of reading page

--- tobe_blog_web/src/portal/containers/content/article/RichContentReader.tsx
+++ tobe_blog_web/src/portal/containers/content/article/RichContentReader.tsx
@@ -1,14 +1,33 @@
 import { Grid } from '@mui/material';
+import { EditorStyle } from '../../../../components/common/EditorStyle';
 import '@wangeditor/editor/dist/css/style.css';
 
 function RichContentReader(props: Readonly<{ htmlValue: string }>) {
   return (
-    <Grid className="w-e-text-container" container sx={{width: '100%'}}>
+    <Grid className="w-e-text-container" container sx={{
+        ...EditorStyle,
+        width: '100%',
+        "& .w-e-text-container ul": {
+          ml: '20px'
+        },
+        "& .w-e-text-container ol": {
+          ml: '20px'
+        },
+        "& .w-e-text-container hr": {
+          margin: '20px auto',
+          border: 'none',
+          borderBottom: '1px solid #e0e0e0',
+        },
+        
+       }}>
         <div
           className='w-e-text-container'
           data-slate-editor
           dangerouslySetInnerHTML={{ __html: props.htmlValue }}
-          style={{width: '100%'}}
+          style={{
+            width: '100%',
+            padding: '0px'
+          }}
         />
     </Grid>
   );
